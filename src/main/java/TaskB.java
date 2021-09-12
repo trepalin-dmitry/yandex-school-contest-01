4284cb4f9db1cd43085954d4963770bef6d9f079
trepalin-dmitry <trepalin.work@mail.ru>
2021-09-12 22:42:36 +0300

    Task B

--- src/main/java/TaskB.java
+++ src/main/java/TaskB.java
@@ -1,21 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TaskB {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        Integer[][] m = new Integer[n][n];
+        var matrix = new BitSet[n][n];
         for (int i = 0; i < n; i++) {
-            m[i] = parseMatrixLine(reader.readLine());
+            matrix[i] = Arrays.stream(reader.readLine().split(" "))
+                    .map(Integer::parseInt)
+                    .map(m -> BitSet.valueOf(new long[]{Long.valueOf(m)}))
+                    .toArray(BitSet[]::new);
         }
-    }
 
-    public static Integer[] parseMatrixLine(String line){
-        return Arrays.stream(line.split(" "))
-                .map(Integer::parseInt)
-                .toArray(Integer[]::new);
+        var result = new BitSet[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = new BitSet();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                var bitSet = matrix[i][j];
+                for (int index = 0; index < bitSet.length(); index++) {
+                    if (bitSet.get(index)) {
+                        result[i].set(index);
+                        result[j].set(index);
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(result).map(m -> {
+                    var array = m.toLongArray();
+                    if (array.length > 0) {
+                        return Math.toIntExact(array[0]);
+                    }
+                    return 0;
+                })
+                .map(m -> Integer.toString(m))
+                .collect(Collectors.joining(" ")));
     }
 }
